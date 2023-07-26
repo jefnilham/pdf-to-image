d1b29d70c3308fd590fa4d846302b4433a5d4777
jefnilham <39832806+jefnilham@users.noreply.github.com>
2023-07-27 05:39:44 +0800

    Add files via upload
    actual code

---
+++ pdf_to_img.py
@@ -0,0 +1,23 @@
+import os 
+from pdf2image import convert_from_path
+
+# download from https://github.com/oschwartz10612/poppler-windows/releases/, unzip, and copy this bin path
+poppler_path = r"C:\Users\Downloads\Release-23.07.0-0\poppler-23.07.0\Library\bin"
+
+# set yourself
+working_path = r"<>"
+
+#place filename here
+filename = r"<>"
+filepath = os.path.join(working_path, filename)
+filename_extensionless = os.path.splitext(str(filename))[0]
+
+pages = convert_from_path(pdf_path=filepath, poppler_path=poppler_path)
+
+i = 1
+for page in pages:
+    image_page = f"page-{i}.jpeg"
+    new_filename = filename_extensionless + "_" + image_page
+    page.save(os.path.join(working_path, new_filename), 'JPEG')
+    i += 1
+
